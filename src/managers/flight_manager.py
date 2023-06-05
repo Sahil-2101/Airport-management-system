b912312c98cc87358a649766a739abaf62c96cec
Sahil-2101 <khatrisahil21@gmail.com>
2023-06-05 10:15:23 +1000

    Added flight status enum and enhanced status management

--- src/managers/flight_manager.py
+++ src/managers/flight_manager.py
@@ -3,6 +3,16 @@ Flight management module for the Airport Management System.
 """
 
 from src.database.connection import DatabaseConnection
+from enum import Enum
+from datetime import datetime, timedelta
+
+class FlightStatus(Enum):
+    """Enum for flight statuses."""
+    SCHEDULED = "SCHEDULED"
+    DELAYED = "DELAYED"
+    CANCELLED = "CANCELLED"
+    DEPARTED = "DEPARTED"
+    ARRIVED = "ARRIVED"
 
 class FlightManager:
     """Handles all flight-related operations."""
@@ -12,21 +22,60 @@ class FlightManager:
 
     def add_flight(self, flight_series: str, flight_number: int, departure: str, 
                   arrival: str, departure_time: str, arrival_time: str, 
-                  total_seats: int, available_seats: int, status: str) -> None:
+                  total_seats: int, available_seats: int) -> None:
         """Add a new flight to the system."""
         query = """INSERT INTO flight (flightseries, flightnumber, departure, arrival, 
                   departuretime, arrivaltime, totalseats, available, status) 
                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         params = (flight_series, flight_number, departure, arrival, 
-                 departure_time, arrival_time, total_seats, available_seats, status)
+                 departure_time, arrival_time, total_seats, available_seats, 
+                 FlightStatus.SCHEDULED.value)
         self.db.execute_query(query, params)
         print("Flight added successfully")
 
-    def update_flight_status(self, flight_series: str, flight_number: int, new_status: str) -> None:
-        """Update the status of a flight (e.g., On Time, Delayed, Cancelled)."""
-        query = "UPDATE flight SET status = %s WHERE flightseries = %s AND flightnumber = %s"
-        self.db.execute_query(query, (new_status, flight_series, flight_number))
-        print("Flight status updated successfully")
+    def update_flight_status(self, flight_series: str, flight_number: int, 
+                           new_status: str, delay_minutes: int = None) -> None:
+        """Update the status of a flight with optional delay information."""
+        try:
+            # Validate the status
+            status = FlightStatus(new_status.upper())
+            
+            # Get current flight details
+            query = "SELECT departuretime, arrivaltime FROM flight WHERE flightseries = %s AND flightnumber = %s"
+            result = self.db.execute_query(query, (flight_series, flight_number))
+            
+            if not result:
+                print("Flight not found")
+                return
+                
+            current_departure = result[0][0]
+            current_arrival = result[0][1]
+            
+            # If delayed, update the times
+            if status == FlightStatus.DELAYED and delay_minutes:
+                new_departure = current_departure + timedelta(minutes=delay_minutes)
+                new_arrival = current_arrival + timedelta(minutes=delay_minutes)
+                query = """UPDATE flight 
+                          SET status = %s, departuretime = %s, arrivaltime = %s 
+                          WHERE flightseries = %s AND flightnumber = %s"""
+                self.db.execute_query(query, (status.value, new_departure, new_arrival, 
+                                           flight_series, flight_number))
+                print(f"Flight delayed by {delay_minutes} minutes")
+            else:
+                query = "UPDATE flight SET status = %s WHERE flightseries = %s AND flightnumber = %s"
+                self.db.execute_query(query, (status.value, flight_series, flight_number))
+                print(f"Flight status updated to {status.value}")
+                
+        except ValueError:
+            print(f"Invalid status. Valid statuses are: {', '.join([s.value for s in FlightStatus])}")
+
+    def get_flight_status(self, flight_series: str, flight_number: int) -> str:
+        """Get the current status of a flight."""
+        query = "SELECT status FROM flight WHERE flightseries = %s AND flightnumber = %s"
+        result = self.db.execute_query(query, (flight_series, flight_number))
+        if result:
+            return result[0][0]
+        return None
 
     def view_flight_schedule(self, date: str = None) -> None:
         """View flight schedule for a specific date or all flights."""
